82e79533e29fd9d73b1b66dab3cae4cddbfe806c
Bryan Marcellinus Kristiono <bmkristiono@uwaterloo.ca>
2017-11-30 22:06:40 -0500

    update grid

--- grid.cc
+++ grid.cc
@@ -33,8 +33,8 @@ void Grid::initGrid() {
 			info.state = StateType::NONE;
 			Cell c = Cell(info);
 			c.attach(td);
-			c.attach(ob);
-			row.emplace_back(Cell(info));
+			if(ob) c.attach(ob);
+			row.emplace_back(c);
 		}
 		// adds each row to the beginning so the bottom left is the ORIGIN (0,0)
 		theGrid.insert(theGrid.begin(), row);
